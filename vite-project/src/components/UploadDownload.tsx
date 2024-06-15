e3857fda7e45f96d570279666530d486bcb60b75
TLRKiliann <philogenie@protonmail.com>
2024-06-15 15:34:18 +0200

    upload download image done

---
+++ vite-project/src/components/UploadDownload.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import "./styles/UploadDownload.css";
+
+const UploadDownload = ({resizerImg, resizeBg, colors}: {
+    resizerImg: number, 
+    resizeBg: number, 
+    colors: string}) => {
+
+    const [selectedImage, setSelectedImage] = useState<File | null>(null);
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files && event.target.files.length > 0) {
+            setSelectedImage(event.target.files[0]);
+        } else {
+            setSelectedImage(null);
+        }
+    };
+
+    const handleRemoveImage = () => {
+        setSelectedImage(null);
+    };
+
+    const downloadImage = () => {
+        if (selectedImage) {
+            const link = document.createElement("a");
+
+            link.href = URL.createObjectURL(selectedImage);
+            link.download = selectedImage.name;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        } else {
+            console.log("No img to download !");
+        }
+    };
+
+    console.log(colors, "colors")
+
+    return (
+        <div className="upload-download-container">
+
+            {selectedImage !== null ? (
+                <div className="img-container">
+                    <div className='img-box' style={{background: `${colors}`}}>
+                        <img
+                            alt="not found"
+                            width={resizerImg}
+                            src={URL.createObjectURL(selectedImage)}
+                            style={{filter: `drop-shadow(10px 
+                                10px ${resizeBg}px rgba(10, 10, 10, 0.8))`}}
+                        />
+                    </div>
+                </div>
+            ) : (
+                <div className='img-container'>
+                </div>
+           )}
+
+            <div className="box-btn">
+                <input
+                    type="file"
+                    name="myImage"
+                    onChange={(event) => handleFileChange(event)}
+                    className="browser-input"
+                />
+                <button onClick={handleRemoveImage}>Remove</button>
+                <button onClick={downloadImage}>Download</button>
+            </div>
+
+            <br />
+
+            <div className="div-browser-input">
+                
+            </div>
+        </div>
+    );
+};
+export default UploadDownload;
